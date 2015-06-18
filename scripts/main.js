57cedd46ce5fc7ca64c667022c887bfbc87df34d
Mitchell Loeppky <mitchell@loeppky.com>
2015-06-17 22:24:10 -0400

    Progress on website

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 var s = skrollr.init();
+
+//The options (second parameter) are all optional. The values shown are the default values.
+skrollr.menu.init(s, {
+
+    //skrollr will smoothly animate to the new position using `animateTo`.
+    animate: true,
+
+    //This event is triggered right before we jump/animate to a new hash.
+    change: function(newHash, newTopPosition) {
+        console.log(newHash);
+        console.log(newTopPosition);
+    }
+});
